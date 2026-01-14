d64018e0b28cac12ad877f2aef7943ea363b9b08
hafizhboyensa <muhammadhafizhboyensa@gmail.com>
2026-01-15 04:04:42 +0700

    Delete

--- 11-november-2025-buku-tamu/bukutamu/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $server = "localhost";
-    $user = "root";
-    $password = "";
-    $nama_database = "db_bukutamu";
-    $db = mysqli_connect($server, $user, $password, $nama_database);
-?>
